15e486ace58d6d730c586c5e22cae34e7005890c
legut <levcenkokola06@gmail.com>
2025-11-02 14:14:25 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import api from "./utils/api"; // твій axios
+import api from "./api"; // твій axios
 
 export default function App() {
   const [userData, setUserData] = useState(null);
