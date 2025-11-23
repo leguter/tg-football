9a5e43b9cfe86d73b58026b94ebb596fb95bd6d9
legut <levcenkokola06@gmail.com>
2025-11-23 15:10:51 +0200

    top left

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -252,7 +252,7 @@ import api from "../../api";
 import styles from "./GamePage.module.css";
 
 const GAME_ANGLES = [
-  { id: 1, name: "Top Left", x: "15%", y: "35%" },
+  { id: 1, name: "Top Left", x: "10%", y: "33%" },
   { id: 2, name: "Top Center", x: "52%", y: "32%" },
   { id: 3, name: "Top Right", x: "77%", y: "35%" },
   { id: 4, name: "Bottom Left", x: "25%", y: "67%" },
