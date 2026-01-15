d22e68d8f39de00426df071099862c7fbd74d658
legut <levcenkokola06@gmail.com>
2026-01-15 18:53:00 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -63,6 +63,7 @@ useEffect(() => {
 }, []);
  // Пустий масив гарантує, що це виконається один раз
 if (userData === null && !authError) {
+  console.log(userData)
   return <div>Загрузка...</div>;
 }
 
